4c3630c474021312bebc37de07158a1656afb923
Pecako2001-PC <koenvanwijlick@gmail.com>
2025-04-21 16:13:21 +0200

    theme bug update

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,17 @@
 // pages/_app.tsx
 import '@mantine/core/styles.css';
 import type { AppProps } from 'next/app';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, createTheme } from '@mantine/core';
 import WebLayout from '../components/WebLayout';
+
+// Optional: customize the theme if needed
+const theme = createTheme({
+  // You can add other theme settings here
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider globalStyles normalizeCSS theme={{}}>
+    <MantineProvider theme={theme}>
       <WebLayout>
         <Component {...pageProps} />
       </WebLayout>
